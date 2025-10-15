9b3cb0621959764d653b9f55fd83ba379f6ccd06
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-15 01:35:12 -0700

    Time: 27 ms (6.97%) | Memory: 9.5 MB (85.29%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1 == "0" || num2 == "0") return "0";
+        int n = num1.size(), m = num2.size();
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        vector<int> res(n + m, 0);
+        for(int i=0; i<num1.size(); i++){
+            int a = num1[i] - '0';
+            for(int j=0; j<num2.size(); j++){
+                int b = num2[j] - '0';
+                res[i + j] += a * b;
+                res[i+j+1] += res[i+j]/10;
+                res[i+j] %= 10;
+                
+            }
+        }
+        while (res.size() > 1 && res.back() == 0) res.pop_back();
+        reverse(res.begin(), res.end());
+        for(int i=0; i<res.size(); i++) cout<<res[i]<<" ";
+        string ans;
+        for (int num : res)
+            ans.push_back(num+'0'); 
+        return ans;
+    }
+};
