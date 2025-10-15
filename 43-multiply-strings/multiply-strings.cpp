0c736ffc30b71f4c1cb76bb75b075db9b5d527a9
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-15 01:49:39 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (85.29%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -3,25 +3,24 @@ public:
     string multiply(string num1, string num2) {
         if(num1 == "0" || num2 == "0") return "0";
         int n = num1.size(), m = num2.size();
+        vector<int> res(n + m, 0);
         reverse(num1.begin(), num1.end());
         reverse(num2.begin(), num2.end());
-        vector<int> res(n + m, 0);
-        for(int i=0; i<num1.size(); i++){
+        for(int i=0; i<n; i++){
             int a = num1[i] - '0';
-            for(int j=0; j<num2.size(); j++){
+            for(int j=0; j<m; j++){
                 int b = num2[j] - '0';
                 res[i + j] += a * b;
-                res[i+j+1] += res[i+j]/10;
-                res[i+j] %= 10;
-                
+                res[i + j + 1] += res[i + j] / 10;
+                res[i + j] %= 10;
             }
         }
-        for(int i=0; i<res.size(); i++) cout<<res[i]<<" ";
         if(res.size() > 1 && res.back() == 0) res.pop_back();
         reverse(res.begin(), res.end());
         string ans;
-        for (int num : res)
-            ans.push_back(num+'0'); 
+        for(auto it: res){
+            ans += it+'0';
+        }
         return ans;
     }
 };
