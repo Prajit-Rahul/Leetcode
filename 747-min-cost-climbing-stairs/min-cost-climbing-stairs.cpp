fe07090dc842d3ffcee8ccc9e103462d6e17bae6
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-25 23:00:19 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.78%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    int recc(vector<int> &cost, int i, vector<int> &dp){
-        // if(i < 0) return 0;
-        if(i == 0 || i == 1) return cost[i];
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = cost[i] + min(recc(cost, i-1, dp), recc(cost, i-2, dp));
-    }
+    // int recc(vector<int> &cost, int i, vector<int> &dp){
+    //     // if(i < 0) return 0;
+    //     if(i == 0 || i == 1) return cost[i];
+    //     if(dp[i] != -1) return dp[i];
+    //     return dp[i] = cost[i] + min(recc(cost, i-1, dp), recc(cost, i-2, dp));
+    // }
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int n = cost.size();
+    //     vector<int> dp(n+1, -1);
+    //     return min(recc(cost, n-1, dp), recc(cost, n-2, dp));
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n+1, -1);
-        return min(recc(cost, n-1, dp), recc(cost, n-2, dp));
+        if(n == 0) return cost[n];
+        vector<int> dp(n+1,0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2; i<n; i++){
+            dp[i] = cost[i] + min(dp[i-2], dp[i-1]);
+        }
+        return min(dp[n-1], dp[n-2]);
     }
+
 };
