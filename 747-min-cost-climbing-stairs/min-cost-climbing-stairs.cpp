3ab9c906b9041172a308c6c3d778f1c30f4a8d5e
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-25 22:44:05 -0700

    Time: 1 ms (13.73%) | Memory: 17.8 MB (11.84%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int recc(vector<int> &cost, int i, vector<int> &dp){
+        if(i >= cost.size()){
+            return 0;
+        }
+        if(dp[i+1] != -1) return dp[i];
+        int left = recc(cost, i+1, dp) + cost[i];
+        int right = recc(cost, i+2, dp) + cost[i];
+        return dp[i] = min(left,right);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size()+1, -1);
+        return min(recc(cost, 0, dp), recc(cost, 1, dp));
+    }
+};
