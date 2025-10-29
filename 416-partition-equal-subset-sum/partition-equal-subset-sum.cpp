7e1fab462c32e109860bed3f763e4c75b01b5f53
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-29 15:48:36 -0700

    Time: 131 ms (55.77%) | Memory: 111.4 MB (14.79%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    bool recc(vector<int> &nums, int ind, vector<vector<int>> &dp, int sum){
-        if(ind >= nums.size()){
-            return false;
-        }
-        if(sum == 0) return true;
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        bool ntake = recc(nums, ind+1, dp, sum);
-        bool take = false;
-        if(nums[ind] <= sum) take = recc(nums, ind+1, dp, sum - nums[ind]);
-        return dp[ind][sum] = (take || ntake);
-    }
+    // bool recc(vector<int> &nums, int ind, vector<vector<int>> &dp, int sum){
+    //     if(ind >= nums.size()){
+    //         return false;
+    //     }
+    //     if(sum == 0) return true;
+    //     if(dp[ind][sum] != -1) return dp[ind][sum];
+    //     bool ntake = recc(nums, ind+1, dp, sum);
+    //     bool take = false;
+    //     if(nums[ind] <= sum) take = recc(nums, ind+1, dp, sum - nums[ind]);
+    //     return dp[ind][sum] = (take || ntake);
+    // }
+    // bool canPartition(vector<int>& nums) {
+    //     int sum = 0;
+    //     int n = nums.size();
+    //     for(int i=0; i<nums.size(); i++){
+    //         sum += nums[i];
+    //     }
+    //     if(sum%2==1) return 0;
+    //     sum /= 2;
+    //     vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+    //     return recc(nums, 0, dp, sum);
+    // }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
@@ -19,7 +30,18 @@ public:
         }
         if(sum%2==1) return 0;
         sum /= 2;
-        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
-        return recc(nums, 0, dp, sum);
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+        for(int ind=n-1; ind>=0; ind--){
+            for(int s = 0; s<=sum; s++){
+                bool ntake = dp[ind+1][s];
+                bool take = false;
+                if(nums[ind] <= s) take = dp[ind+1][s - nums[ind]];
+                dp[ind][s] = (take || ntake);
+            }
+        }
+        return dp[0][sum];
     }
 };
