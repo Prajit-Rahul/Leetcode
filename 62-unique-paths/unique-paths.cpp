9da3be05c36a9147e47bbaede031ea43b59463ae
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-29 21:35:14 -0700

    Time: 0 ms (100.00%) | Memory: 8.3 MB (80.53%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -12,18 +12,33 @@ public:
     //     vector<vector<int>> dp(m, vector<int>(n, -1));
     //     return recc(m-1, n-1, dp);
     // }
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int>(n, 0));
+    //     for(int i=0; i<m; i++){
+    //         for(int j=0; j<n; j++){
+    //             if(i == 0 && j == 0) dp[i][j] = 1;
+    //             else{
+    //                 int top = (i-1>=0)?dp[i-1][j]:0;
+    //                 int bottom = (j-1>=0)?dp[i][j-1]:0;
+    //                 dp[i][j] = top + bottom;
+    //             }
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+    // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> curr(n, 0), prev(n, 0);
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i == 0 && j == 0) dp[i][j] = 1;
+                if(i == 0 && j == 0) curr[j] = 1;
                 else{
-                    int top = (i-1>=0)?dp[i-1][j]:0;
-                    int bottom = (j-1>=0)?dp[i][j-1]:0;
-                    dp[i][j] = top + bottom;
+                    int top = (i-1>=0)?prev[j]:0;
+                    int bottom = (j-1>=0)?curr[j-1]:0;
+                    curr[j] = top + bottom;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
