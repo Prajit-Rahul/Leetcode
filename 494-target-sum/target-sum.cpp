e1eeb4f2618aaab0d36452f46941a934261410fa
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-30 15:02:11 -0700

    Time: 8 ms (64.92%) | Memory: 15.2 MB (57.86%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int recc(vector<int>& nums, int ind, int sum, vector<vector<int>> &dp){
-        if(ind <= 0){
-            if(nums[ind] == 0 && sum == 0) return 2;
-            if(sum == 0 || sum == nums[ind]) return 1;
-            return 0;
-        }
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        int take = (sum-nums[ind]>=0)?recc(nums, ind-1, sum-nums[ind], dp):0;
-        int ntake = recc(nums, ind-1, sum, dp);
-        return dp[ind][sum] = take + ntake;
-    }
+    // int recc(vector<int>& nums, int ind, int sum, vector<vector<int>> &dp){
+    //     if(ind <= 0){
+    //         if(nums[ind] == 0 && sum == 0) return 2;
+    //         if(sum == 0 || sum == nums[ind]) return 1;
+    //         return 0;
+    //     }
+    //     if(dp[ind][sum] != -1) return dp[ind][sum];
+    //     int take = (sum-nums[ind]>=0)?recc(nums, ind-1, sum-nums[ind], dp):0;
+    //     int ntake = recc(nums, ind-1, sum, dp);
+    //     return dp[ind][sum] = take + ntake;
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     int sum = 0;
+    //     int n = nums.size();
+    //     for(int i=0; i<nums.size(); i++){
+    //         sum += nums[i];
+    //     }
+    //     sum = sum - target;
+    //     if(sum < 0 || sum%2!=0) return 0;
+    //     sum /= 2;
+    //     vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+    //     return recc(nums, n-1, sum, dp);
+    // }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum = 0;
         int n = nums.size();
@@ -20,7 +32,16 @@ public:
         sum = sum - target;
         if(sum < 0 || sum%2!=0) return 0;
         sum /= 2;
-        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
-        return recc(nums, n-1, sum, dp);
+        vector<vector<unsigned>> dp(n, vector<unsigned>(sum+1, 0));
+        dp[0][0] = 1;
+        if(sum >= nums[0]) dp[0][nums[0]] = 1;
+        if(nums[0] == 0) dp[0][0] = 2;
+        for(int ind=1; ind<n; ind++){
+            for(int s=0; s<=sum; s++){
+                dp[ind][s] += (s-nums[ind]>=0)?dp[ind-1][s-nums[ind]]:0;
+                dp[ind][s] += dp[ind-1][s];
+            }
+        }
+        return dp[n-1][sum];
     }
 };
