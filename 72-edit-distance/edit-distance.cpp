a84862e2d15b129e05480423bef2c971814a9e66
Prajit Rahul <prajitrahul05@gmail.com>
2025-10-31 09:58:33 -0700

    Time: 7 ms (61.03%) | Memory: 13.3 MB (37.08%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    int recc(string &word1, string &word2, int ind1, int ind2, vector<vector<int>> &dp){
-        if(ind2 < 0) return ind1+1;
-        if(ind1 < 0) return ind2+1; 
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        int noOp = INT_MAX, Op = INT_MAX;
-        if(word1[ind1] == word2[ind2]) noOp = recc(word1, word2, ind1-1, ind2-1, dp);
-        else{
-        int insert = 1 + recc(word1, word2, ind1, ind2-1, dp);
-        int del = 1 + recc(word1, word2, ind1-1, ind2, dp);
-        int update = 1 + recc(word1, word2, ind1-1, ind2-1, dp);
-        Op = min({insert, del, update});
-        }
-        return dp[ind1][ind2] = min(noOp, Op);
-    }
+    // int recc(string &word1, string &word2, int ind1, int ind2, vector<vector<int>> &dp){
+    //     if(ind2 < 0) return ind1+1;
+    //     if(ind1 < 0) return ind2+1; 
+    //     if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+    //     int noOp = INT_MAX, Op = INT_MAX;
+    //     if(word1[ind1] == word2[ind2]) noOp = recc(word1, word2, ind1-1, ind2-1, dp);
+    //     else{
+    //     int insert = 1 + recc(word1, word2, ind1, ind2-1, dp);
+    //     int del = 1 + recc(word1, word2, ind1-1, ind2, dp);
+    //     int update = 1 + recc(word1, word2, ind1-1, ind2-1, dp);
+    //     Op = min({insert, del, update});
+    //     }
+    //     return dp[ind1][ind2] = min(noOp, Op);
+    // }
+    // int minDistance(string word1, string word2) {
+    //     int n = word1.size();
+    //     int m = word2.size();
+    //     vector<vector<int>> dp(n, vector<int>(m, -1));
+    //     return recc(word1, word2, n-1, m-1, dp);
+    // }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return recc(word1, word2, n-1, m-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i;
+        }
+        for(int i=0; i<=m; i++){
+            dp[0][i] = i;
+        }
+        for(int ind1=1; ind1<=n; ind1++){
+            for(int ind2=1; ind2<=m; ind2++){
+                int noOp = INT_MAX, Op = INT_MAX;
+                if(word1[ind1-1] == word2[ind2-1]) noOp = dp[ind1-1][ind2-1];
+        else{
+        int insert = 1 + dp[ind1][ind2-1];
+        int del = 1 + dp[ind1-1][ind2];
+        int update = 1 + dp[ind1-1][ind2-1];
+        Op = min({insert, del, update});
+        }
+        dp[ind1][ind2] = min(noOp, Op);
+            }
+        }
+        return dp[n][m];
     }
 };
