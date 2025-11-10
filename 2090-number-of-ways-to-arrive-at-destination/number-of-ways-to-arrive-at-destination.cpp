a6ef299c251cef6460686f87ca14fa2c9bf3d1ce
Prajit Rahul <prajitrahul05@gmail.com>
2025-11-10 13:16:57 -0800

    Time: 8 ms (59.39%) | Memory: 38.1 MB (21.04%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,36 @@
+#define ll long long
+#define pll pair<long, long>
+class Solution {
+public:
+    ll MOD = 1e9 + 7;
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<ll> dist(n, LLONG_MAX);
+        vector<ll> ways(n, 0);
+        vector<vector<pll>> adj(n);
+        for(auto &it: roads){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        priority_queue<pll, vector<pll>, greater<pll>> pq;
+        pq.push({0, 0});
+        dist[0] = 0;
+        ways[0] = 1;
+        while(!pq.empty()){
+            auto [wt,node] = pq.top();
+            pq.pop();
+            for(auto &it: adj[node]){
+                ll edgeWt = it.second;
+                ll adjNode = it.first;
+                if(wt + edgeWt < dist[adjNode]){
+                    dist[adjNode] = wt + edgeWt;
+                    ways[adjNode] = ways[node];
+                    pq.push({dist[adjNode], adjNode});
+                }
+                else if(wt + edgeWt == dist[adjNode]){
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % MOD;
+                }
+            }
+        }
+        return ways[n-1];
+    }
+};
