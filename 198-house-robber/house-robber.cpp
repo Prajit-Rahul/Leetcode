22d1e857e8a0b33f204c03001f9f195d193984d2
Prajit Rahul <prajitrahul05@gmail.com>
2025-11-18 00:12:30 -0800

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.63%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-
-    // int recc(vector<int> &nums, int i, vector<int> &dp){
-    //     if(i >= nums.size()) return 0;
-    //     if(dp[i] != -1) return dp[i];
-    //     int p = recc(nums, i+2, dp) + nums[i];
-    //     int np = recc(nums, i+1, dp);
-    //     return dp[i] = max(p, np);
-    // }
+    int recc(vector<int>& nums, int ind, vector<int> &dp){
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int take = recc(nums, ind-2, dp) + nums[ind];
+        int ntake = recc(nums, ind-1, dp);
+        return dp[ind] = max(take, ntake);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, 0);
-        dp[0] = nums[0];
-        for(int i=1; i<nums.size(); i++){
-            int take = nums[i];
-            if(i-2>=0) take += dp[i-2];
-            int ntake = dp[i-1];
-            dp[i] = max(take, ntake);
-        }
-        return dp[n-1];
+        vector<int> dp(n, -1);
+        return recc(nums, n-1, dp);
     }
 };
