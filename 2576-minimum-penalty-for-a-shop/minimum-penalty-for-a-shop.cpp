42e557f685f5bac5a55ac6122a49f40e67a005b7
Prajit Rahul <prajitrahul05@gmail.com>
2025-12-31 21:57:25 -0800

    Time: 8 ms (39.58%) | Memory: 21.8 MB (13.26%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -3,25 +3,29 @@ public:
     int bestClosingTime(string customers) {
         int n = customers.length();
         vector<int> open(n+1, 0), close(n+1, 0);
-        int j = 0, k = 0;
+        int minPen = n, currPen = 0;
+        for(auto &it: customers){
+            if(it == 'Y'){
+                currPen++;
+            }
+        }
+        if(minPen > currPen){
+            minPen = currPen;
+        }
+        int ans = 0;
         for(int i=0; i<n; i++){
-            if(customers[n-i-1] == 'Y'){
-                j++;
+            auto it = customers[i];
+            if(it == 'Y'){
+                currPen--;
             }
-            if(customers[i] == 'N'){
-                k++;
+            else{
+                currPen++;
             }
-            open[n-i-1] = j;
-            close[i+1] = k;
-        }
-        int ansInd = 0, ans = INT_MAX;
-        for(int i=0; i<=n; i++){
-            int pen = open[i] + close[i];
-            if(pen < ans){
-                ans = pen;
-                ansInd = i;
+            if(minPen > currPen){
+                minPen = currPen;
+                ans = i+1;
             }
         }
-        return ansInd;
+        return ans;
     }
 };
