fc0076d04a91d738ea42be97a39a69d641204566
Prajit Rahul <prajitrahul05@gmail.com>
2026-01-04 06:30:18 -0800

    Time: 286 ms (13.76%) | Memory: 91.9 MB (9.39%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        const int MOD = 1000000007;
-        long long A = 6, B = 6;
-
-        for (int i = 2; i <= n; i++) {
-            long long newA = (2 * A + 2 * B) % MOD;
-            long long newB = (2 * A + 3 * B) % MOD;
-            A = newA;
-            B = newB;
+    const int mod = 1e9 + 7; 
+    int solve(int ind, int n, int prev1, int prev2, int prev3, vector<vector<vector<vector<int>>>> &dp){
+        if(ind == n) return 1;
+        if(dp[ind][prev1+1][prev2+1][prev3+1] != -1) return dp[ind][prev1+1][prev2+1][prev3+1];
+        int ans = 0;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                for(int k=0; k<3; k++){
+                    if(i != j && j != k && i != prev1 && j != prev2 && k != prev3){
+                        ans = (ans + solve(ind+1, n, i, j, k, dp))%mod;
+                    }
+                }
+            }
         }
-
-        return (A + B) % MOD;
+        return dp[ind][prev1+1][prev2+1][prev3+1] = ans;
+    }
+    int numOfWays(int n) {
+        vector<vector<vector<vector<int>>>> dp(n+1, vector<vector<vector<int>>>(4, vector<vector<int>>(4, vector<int>(4, -1))));
+        return solve(0, n, -1, -1, -1, dp);
     }
 };
