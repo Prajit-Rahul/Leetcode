b969e303ad708d7909424e4d01449cf019dddff1
Prajit Rahul <prajitrahul05@gmail.com>
2026-01-16 15:06:16 -0800

    Time: 0 ms (100.00%) | Memory: 9.9 MB (10.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        unordered_map<int,int> mp;
+        for(int i=0; i<s.length(); i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto &it: mp){
+            if(it.second != 0) return false;
+        }
+        return true;
+    }
+};
