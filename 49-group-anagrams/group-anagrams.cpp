9ae477c126eb1b028b738a458414031540f8fac7
Prajit Rahul <prajitrahul05@gmail.com>
2026-01-17 03:19:14 -0800

    Time: 11 ms (90.69%) | Memory: 24.9 MB (85.29%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
+    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    //     vector<pair<unordered_map<char, int>, vector<string>>> store;
+    //     for(auto &str: strs){
+    //         unordered_map<char,int> mp;
+    //         for(auto &c: str){
+    //             mp[c]++;
+    //         }
+    //         bool found = false;
+    //         for(auto &it: store){
+    //             if(it.first == mp){
+    //                 it.second.push_back(str);
+    //                 found = true;
+    //             }
+    //         }
+    //         if(!found){
+    //             store.push_back({mp, {str}});
+    //         }
+    //     }
+    //     vector<vector<string>> ans;
+    //     for(auto &it: store){
+    //         ans.push_back(it.second);
+    //     }
+    //     return ans;
+    // }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<pair<unordered_map<char, int>, vector<string>>> store;
+        unordered_map<string, vector<string>> v;
         for(auto &str: strs){
-            unordered_map<char,int> mp;
-            for(auto &c: str){
-                mp[c]++;
-            }
-            bool found = false;
-            for(auto &it: store){
-                if(it.first == mp){
-                    it.second.push_back(str);
-                    found = true;
-                }
-            }
-            if(!found){
-                store.push_back({mp, {str}});
-            }
+            string key = str;
+            sort(key.begin(), key.end());
+            v[key].push_back(str);
         }
-        vector<vector<string>> ans;
-        for(auto &it: store){
-            ans.push_back(it.second);
+        vector<vector<string>> result;
+        for(auto &it: v){
+            result.push_back(it.second);
         }
-        return ans;
+        return result;
     }
 };
