25ecd838a264612839cd79391a3d9ce7b57a6207
Prajit Rahul <prajitrahul05@gmail.com>
2026-01-18 14:27:38 -0800

    Time: 11 ms (47.44%) | Memory: 11.9 MB (53.36%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
+    // int lengthOfLongestSubstring(string s) {
+    //     int l = 0, r = 0, maxLen = 0;
+    //     int n = s.length();
+    //     unordered_map<char, int> mp;
+    //     while(r<n){
+    //         if(mp.find(s[r]) == mp.end()){
+    //             mp[s[r]]++;
+    //         }
+    //         else{
+    //             while(mp[s[r]] > 0){
+    //                 mp.erase(s[l]);
+    //                 l++;
+    //             }
+    //             mp[s[r]]++;
+    //         }
+    //         maxLen = max(maxLen, r - l + 1);
+    //         r++;
+    //     }
+    //     return maxLen;
+    // }
     int lengthOfLongestSubstring(string s) {
         int l = 0, r = 0, maxLen = 0;
         int n = s.length();
         unordered_map<char, int> mp;
         while(r<n){
-            if(mp.find(s[r]) == mp.end()){
-                mp[s[r]]++;
-            }
-            else{
-                while(mp[s[r]] > 0){
-                    mp.erase(s[l]);
-                    l++;
-                }
-                mp[s[r]]++;
+            if(mp.find(s[r]) != mp.end()){
+                l =  max(l, mp[s[r]] + 1);
             }
             maxLen = max(maxLen, r - l + 1);
+            mp[s[r]] = r;
             r++;
         }
         return maxLen;
