f11d70dae7d4b36ff516a79a7b015cd4d22f9430
Prajit Rahul <prajitrahul05@gmail.com>
2026-01-30 18:58:48 -0800

    Time: 0 ms (100.00%) | Memory: 10.6 MB (50.14%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void recc(vector<int>& nums, int ind, vector<int> &num){
-        ans.push_back(num);    
+    void recc(vector<int> &nums, int ind, vector<int> &temp, vector<vector<int>> &ans){
+        ans.push_back(temp);
         for(int i=ind; i<nums.size(); i++){
             if(i > ind && nums[i] == nums[i-1]) continue;
-            num.push_back(nums[i]);
-            recc(nums, i+1, num);
-            num.pop_back();
+            temp.push_back(nums[i]);
+            recc(nums, i+1, temp, ans);
+            temp.pop_back();
         }
-        
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
         sort(nums.begin(), nums.end());
-        vector<int> num;
-        recc(nums, 0, num);
+        recc(nums, 0, temp, ans);
         return ans;
     }
 };
