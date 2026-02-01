b8e6c6e4d6e9e8a69165889a9695c652dcb36329
Prajit Rahul <prajitrahul05@gmail.com>
2026-01-31 17:02:25 -0800

    Time: 0 ms (100.00%) | Memory: 17.2 MB (19.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,39 +3,38 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        queue<pair<int,pair<int,int>>> q;
+        queue<pair<int, pair<int,int>>> q;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == 2){
-                    vis[i][j] = 0;
-                    q.push({0, {i, j}});
+                    vis[i][j] = 1;
+                    q.push({0,{i,j}});
                 }
             }
         }
-        int dr[4] = {1,0,-1,0};
-        int dc[4] = {0,1,0,-1};
+        vector<int> dr = {0,-1,0,1};
+        vector<int> dc = {1,0,-1,0};
         int maxTime = 0;
         while(!q.empty()){
             int time = q.front().first;
-            int r = q.front().second.first;
-            int c = q.front().second.second;
+            auto [r, c] = q.front().second;
             q.pop();
-            maxTime = max(time, 0);
+            maxTime = max(maxTime, time);
             for(int i=0; i<4; i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr < 0 || nr >= n || nc < 0 || nc >= m || grid[nr][nc] != 1) continue;
-                q.push({time+1, {nr, nc}});
+                if(nr < 0 || nc < 0 || nr >= n || nc >= m || vis[nr][nc] || grid[nr][nc] != 1) continue;
                 vis[nr][nc] = 1;
-                grid[nr][nc] = 2;
+                q.push({time+1,{nr,nc}});
             }
         }
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 1){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     return -1;
                 }
             }
-        }        return maxTime;
+        }
+        return maxTime;
     }
 };
