397b20e158c6ef125e00e8604405eaa3f4053f4f
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-02 23:17:07 -0800

    Time: 0 ms (100.00%) | Memory: 10.2 MB (75.50%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -23,21 +23,24 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int> dp1(n+1,0), dp2(n+1,0);
+        int prev1 = 0, prev2 = 0;
         for(int i=1; i<n; i++){
             int take = nums[i];
-            if(i>=2) take += dp1[i-2];
+            if(i>=2) take += prev2;
             int ntake = 0;
-            if(i>=1) ntake += dp1[i-1];
-            dp1[i] = max(take, ntake);
+            if(i>=1) ntake += prev1;
+            prev2 = prev1;
+            prev1 = max(take, ntake);
         }
+        int prev3 = 0, prev4 = 0;
         for(int i=0; i<n-1; i++){
             int take = nums[i];
-            if(i>=2) take += dp2[i-2];
+            if(i>=2) take += prev4;
             int ntake = 0;
-            if(i>=1) ntake += dp2[i-1];
-            dp2[i] = max(take, ntake);
+            if(i>=1) ntake += prev3;
+            prev4 = prev3;
+            prev3 = max(take, ntake);
         }
-        return max(dp1[n-1], dp2[n-2]);
+        return max(prev1, prev3);
     }
 };
