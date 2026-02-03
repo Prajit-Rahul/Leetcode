211f6461e816a647c2f59317a1f8ad37319ddc49
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-03 13:14:29 -0800

    Time: 47 ms (33.46%) | Memory: 53.3 MB (10.78%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,22 @@
 class Solution {
-public:
-    int mini = INT_MAX;
-    int recc(vector<int>& coins, int amount, int ind, vector<vector<int>> &dp){
+public: 
+
+    int recc(vector<int> &coins, int amount, int ind, vector<vector<int>> &dp){
         if(ind == 0){
-            if(amount % coins[ind] == 0) return amount/coins[ind];
+            if(amount % coins[0] == 0) return amount/coins[0];
             return 1e9;
         }
-        if(dp[amount][ind] != -1) return dp[amount][ind];
-        int take = (amount - coins[ind]>=0)? 1 + recc(coins, amount - coins[ind], ind, dp):INT_MAX;
+        if(dp[ind][amount] != -1) return dp[ind][amount];
         int ntake = recc(coins, amount, ind-1, dp);
-        dp[amount][ind] = min(take, ntake);
-        mini = min(mini, dp[amount][ind]);
-        return dp[amount][ind];
+        int take = INT_MAX;
+        if(coins[ind] <= amount) take = 1 + recc(coins,  amount - coins[ind], ind, dp);
+        dp[ind][amount] = min(take, ntake);
+        return dp[ind][amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        const int INF = 1e9;
         int n = coins.size();
-        vector<vector<int>> dp(amount+1, vector<int>(n,-1));
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
         int ans = recc(coins, amount, n-1, dp);
-        return (ans == INF)?-1:ans;
+        return ans == 1e9?-1:ans;
     }
 };
