2af329cac720bf32d4995b0b326590ed5b9be761
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-03 19:02:20 -0800

    Time: 271 ms (34.55%) | Memory: 296.4 MB (20.60%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // int recc(vector<int> &nums, int ind, int prev, vector<vector<int>> &dp){
-    //     if(ind >= nums.size()) return 0;
+    //     if(ind == nums.size()) return 0;
     //     if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
     //     int ntake = recc(nums, ind+1, prev, dp);
     //     int take = INT_MIN;
@@ -10,64 +10,20 @@ public:
     // }
     // int lengthOfLIS(vector<int>& nums) {
     //     int n = nums.size();
-    //     vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+    //     vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
     //     return recc(nums, 0, -1, dp);
-    // }
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-    //     for(int ind=n-1; ind>=0; ind--){
-    //         for(int prev=ind-1; prev >= -1; prev--){
-    //             int ntake = dp[ind+1][prev+1];
-    //             int take = 0;
-    //             if(prev == -1 || nums[ind] > nums[prev]) take = 1 + dp[ind+1][ind+1];
-    //             dp[ind][prev+1] = max(take, ntake);
-    //         }
-    //     }
-    //     return dp[0][0];
-    // }
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<int> curr(n+1, 0), next(n+1, 0);
-    //     for(int ind=n-1; ind>=0; ind--){
-    //         for(int prev=ind-1; prev >= -1; prev--){
-    //             int ntake = curr[prev+1];
-    //             int take = 0;
-    //             if(prev == -1 || nums[ind] > nums[prev]) take = 1 + curr[ind+1];
-    //             next[prev+1] = max(take, ntake);
-    //         }
-    //         curr = next;
-    //     }
-    //     return next[0];
-    // }
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<int> dp(n+1, 1);
-    //     int maxi = 1;
-    //     for(int ind=0; ind<=n-1; ind++){
-    //         for(int prev=0; prev<ind; prev++){
-    //             if(nums[ind] > nums[prev]){
-    //                 dp[ind] = max(dp[ind], dp[prev] + 1);
-    //                 maxi = max(dp[ind], maxi);
-    //             }
-    //         }
-    //     }
-    //     return maxi;
     // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> sub;
-        int maxi = 0;
-        for(int ind=0; ind<=n-1; ind++){
-            if(sub.size() == 0 || sub.back() < nums[ind]){
-                sub.push_back(nums[ind]);
-                maxi++;
-            }
-            else{
-                auto it = lower_bound(sub.begin(), sub.end(), nums[ind]);
-                *it = nums[ind];
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        for(int ind=n-1; ind>=0; ind--){
+            for(int prev = ind-1; prev >= -1; prev--){
+                int ntake = dp[ind+1][prev+1];
+                int take = INT_MIN;
+                if(prev == -1 || nums[ind] > nums[prev]) take = 1 + dp[ind+1][ind+1];
+                dp[ind][prev+1] = max(take, ntake);
             }
         }
-        return maxi;
+        return dp[0][0];
     }
 };
