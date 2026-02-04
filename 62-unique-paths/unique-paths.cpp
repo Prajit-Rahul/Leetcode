c88c1d07cfed37bd2a16b024e755203f1b30f3d3
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-03 22:20:52 -0800

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.64%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,44 +1,15 @@
 class Solution {
 public:
-    // int recc(int i, int j, vector<vector<int>> &dp){
-    //     if(i < 0 || j < 0) return 0;
-    //     if(i == 0 && j == 0) return 1;
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     int top = recc(i-1, j, dp);
-    //     int bottom = recc(i, j-1, dp);
-    //     return dp[i][j] = top + bottom;
-    // }
-    // int uniquePaths(int m, int n) {
-    //     vector<vector<int>> dp(m, vector<int>(n, -1));
-    //     return recc(m-1, n-1, dp);
-    // }
-    // int uniquePaths(int m, int n) {
-    //     vector<vector<int>> dp(m, vector<int>(n, 0));
-    //     for(int i=0; i<m; i++){
-    //         for(int j=0; j<n; j++){
-    //             if(i == 0 && j == 0) dp[i][j] = 1;
-    //             else{
-    //                 int top = (i-1>=0)?dp[i-1][j]:0;
-    //                 int bottom = (j-1>=0)?dp[i][j-1]:0;
-    //                 dp[i][j] = top + bottom;
-    //             }
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
-    // }
+    int recc(int m, int n, vector<vector<int>> &dp){
+        if(n == 0 && m == 0) return 1;
+        if(m < 0 || n < 0) return 0;
+        if(dp[m][n] != -1) return dp[m][n];
+        int top = recc(m-1, n, dp);
+        int left = recc(m, n-1, dp);
+        return dp[m][n] = top + left;
+    }
     int uniquePaths(int m, int n) {
-        vector<int> curr(n, 0), prev(n, 0);
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i == 0 && j == 0) curr[j] = 1;
-                else{
-                    int top = (i-1>=0)?prev[j]:0;
-                    int bottom = (j-1>=0)?curr[j-1]:0;
-                    curr[j] = top + bottom;
-                }
-            }
-            prev = curr;
-        }
-        return prev[n-1];
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return recc(m-1, n-1, dp);
     }
 };
