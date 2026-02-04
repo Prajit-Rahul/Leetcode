f06e4ea74fbbe18ae3e739079a6a9e86916431fb
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-04 01:20:36 -0800

    Time: 142 ms (5.02%) | Memory: 29.1 MB (6.36%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
-    // int recc(string &text1, string &text2, int ind1, int ind2, vector<vector<int>> &dp){
-    //     if(ind1 == text1.length() || ind2 == text2.length()){
-    //         return 0;
-    //     }
-    //     if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-    //     int len = 0, ntake1 = 0, ntake2=0; 
-    //     if(text1[ind1] == text2[ind2]){
-    //         len = 1 + recc(text1, text2, ind1+1, ind2+1, dp);
-    //     }
-    //     else{
-    //     ntake1 = recc(text1, text2, ind1+1, ind2, dp);
-    //     ntake2 = recc(text1, text2, ind1, ind2+1, dp);
-    //     }
-    //     return dp[ind1][ind2] = max({len, ntake1, ntake2});
-    // }
-    // int longestCommonSubsequence(string text1, string text2) {
-    //     int n = text1.size(), m = text2.size();
-    //     vector<vector<int>> dp(n, vector<int>(m, -1));
-    //     return recc(text1, text2, 0, 0, dp);
-    // }
+    int recc(string &text1, string &text2, int ind1, int ind2, vector<vector<int>> &dp){
+        if(ind1 < 0 || ind2 < 0) return 0;
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        int btake = 0;
+        if(text1[ind1] == text2[ind2]) btake = 1 + recc(text1, text2, ind1-1, ind2-1, dp);
+        int ntake1 = recc(text1, text2, ind1-1, ind2, dp);
+        int ntake2 = recc(text1, text2, ind1, ind2-1, dp);
+        return dp[ind1][ind2] = max({btake, ntake1, ntake2});
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int ind1=n-1; ind1>=0; ind1--){
-            for(int ind2=m-1; ind2>=0; ind2--){
-                int len = 0, ntake1 = 0, ntake2=0; 
-                if(text1[ind1] == text2[ind2]){
-                    len = 1 + dp[ind1+1][ind2+1];
-                }
-                ntake1 = dp[ind1+1][ind2];
-                ntake2 = dp[ind1][ind2+1];
-                dp[ind1][ind2] = max({len, ntake1, ntake2});
-            }
-        }
-        return dp[0][0];
+        int n = text1.length(), m = text2.length();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return recc(text1, text2, n-1, m-1, dp);
     }
 };
