0945230dd7944ac1759a743b931fc34476908aa7
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-05 15:53:34 -0800

    Time: 31 ms (52.64%) | Memory: 27.1 MB (53.31%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -5,15 +5,12 @@ public:
         if(ind1 < 0) return 0;
         if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
         int take = 0;
-        if(s[ind1] == t[ind2]){
-            take = recc(s, t, ind1-1, ind2-1, dp);
-        }
+        if(s[ind1] == t[ind2]) take += recc(s, t, ind1-1, ind2-1, dp);
         int ntake = recc(s, t, ind1-1, ind2, dp);
         return dp[ind1][ind2] = take + ntake;
     }
     int numDistinct(string s, string t) {
-        int n = s.length();
-        int m = t.length();
+        int n = s.length(), m = t.length();
         vector<vector<int>> dp(n, vector<int>(m, -1));
         return recc(s, t, n-1, m-1, dp);
     }
