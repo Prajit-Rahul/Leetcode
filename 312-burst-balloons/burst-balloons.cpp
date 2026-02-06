563e67be1e5f479cd415e43444424e8b1dc8e9cf
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-06 11:51:29 -0800

    Time: 201 ms (38.32%) | Memory: 14.1 MB (7.64%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int recc(vector<int>& nums, int i, int j, vector<vector<int>> &dp){
-        if(i>j) return 0;
+    int recc(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
+        if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int maxi = 0;
-        for(int k=i; k<=j; k++){
-            int cost = nums[i-1]*nums[j+1]*nums[k] + recc(nums, i, k-1, dp) + recc(nums, k+1, j, dp);
-            maxi = max(maxi, cost);
+        for(int ind = i; ind <= j; ind++){
+            int cost = nums[i-1] * nums[ind] * nums[j+1] + recc(nums, i, ind-1, dp) + recc(nums, ind+1, j, dp);
+            maxi = max(cost, maxi);
         }
         return dp[i][j] = maxi;
     }
     int maxCoins(vector<int>& nums) {
-        int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
         int c = nums.size();
