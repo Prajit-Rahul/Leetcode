13b8644d5fc3cca669adbaef01681f7d16fa2323
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-06 15:28:56 -0800

    Time: 811 ms (5.00%) | Memory: 9.7 MB (16.82%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -5,20 +5,20 @@ public:
         if(ind2 >= m){
             return (ind1 == n);
         }
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        bool select = false, notSelect = false;
-        bool match = ind1 < n && (s[ind1] == p[ind2] || p[ind2] == '.');
-        if(match)
-            select = recc(s, p, ind1+1, ind2+1, dp);
-        if((ind2 + 1) < m && p[ind2+1] == '*'){
-            notSelect = recc(s, p, ind1, ind2+2, dp) ||
-                (match && recc(s, p, ind1+1, ind2, dp));
+        bool ans = false;
+        bool match = ind1 < n && ((s[ind1] == p[ind2]) || p[ind2] == '.');
+        if(match){
+            ans = ans || recc(s, p, ind1+1, ind2+1, dp);
         }
-        return dp[ind1][ind2] = (select || notSelect);
+        if(ind2+1 < m && p[ind2+1] == '*'){
+            ans = ans || recc(s, p, ind1, ind2+2, dp);
+            ans = ans || (match && recc(s, p, ind1+1, ind2, dp));
+        }
+        return dp[ind1][ind2] = ans;
     }
     bool isMatch(string s, string p) {
         int n = s.length(), m = p.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
         return recc(s, p, 0, 0, dp);
     }
 };
