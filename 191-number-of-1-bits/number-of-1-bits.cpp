d5bf3d2ec27f051ee07a9ddb0b1f94ac88113e69
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-08 22:50:46 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.73%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -8,11 +8,19 @@ public:
     //     }
     //     return count;
     // }
+    // int hammingWeight(int n) {
+    //     int count = 0;
+    //     while(n){
+    //         count += n % 2;
+    //         n >>= 1;
+    //     }
+    //     return count;
+    // }
     int hammingWeight(int n) {
         int count = 0;
         while(n){
-            count += n % 2;
-            n >>= 1;
+            n = n & (n-1);
+            count++;
         }
         return count;
     }
