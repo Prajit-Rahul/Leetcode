f0ed80c26b2823566ec23656ac53820b1abf978f
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-09 22:36:34 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.50%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -5,29 +5,27 @@ public:
         for(auto &it: flights){
             adj[it[0]].push_back({it[1], it[2]});
         }
-        // priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<>> pq;
-        queue<pair<int,pair<int,int>>> pq;
+        // if(k==0) return -1;
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{src, 0}});
         vector<int> dist(n, INT_MAX);
         dist[src] = 0;
-        pq.push({0,{src, 0}});
-        while(!pq.empty()){
-            // int u = pq.top().second.first;
-            // int stops = pq.top().second.second;
-            // int wt = pq.top().first;
-            int u = pq.front().second.first;
-            int stops = pq.front().second.second;
-            int wt = pq.front().first;
-            pq.pop();
+        while(!q.empty()){
+            int stops = q.front().first;
+            int node = q.front().second.first;
+            int wt = q.front().second.second;
+            q.pop();
             if(stops > k) continue;
-            for(auto &it: adj[u]){
+            for(auto &it: adj[node]){
                 int edgeWt = it.second;
-                int v = it.first;
-                if(wt + edgeWt < dist[v] && stops <= k){
-                    dist[v] = wt + edgeWt;
-                    pq.push({dist[v], {v, stops+1}});
+                int adjNode = it.first;
+                if(wt + edgeWt < dist[adjNode]){
+                    dist[adjNode] = wt + edgeWt ;
+                    q.push({stops+1,{adjNode, dist[adjNode]}});
                 }
             }
         }
-        return dist[dst]==INT_MAX ? -1:dist[dst]; 
+        if(dist[dst] == INT_MAX) return -1;
+        return dist[dst];
     }
 };
