66def88b35fab0bab321ae1fa9fc30cef37ba99f
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-10 12:02:26 -0800

    Time: 0 ms (100.00%) | Memory: 17.1 MB (28.60%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -3,13 +3,10 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-        for(int i=0; i<2*n; i++){
-            if(i < n){
-                ans.push_back(nums[i]);
-            }
-            else{
-                ans.push_back(nums[i-n]);
-            }
+        for(int j=0; j<2; j++){
+        for(int i=0; i<n; i++){
+            ans.push_back(nums[i]);
+        }
         }
         return ans;
     }
