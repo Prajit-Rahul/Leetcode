e8634f22e4bd54eece41d3a397bae651ca273a24
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-09 20:15:38 -0800

    Time: 11 ms (29.34%) | Memory: 19.6 MB (95.40%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string,multiset<string>> adj;
-        vector<string> ans;
-        int n = tickets.size();
+        unordered_map<string, multiset<string>> mp;
         for(auto &it: tickets){
-            adj[it[0]].insert(it[1]);
+            mp[it[0]].insert(it[1]);
         }
-        string s = "JFK";
-        stack<string> mystack;
-        mystack.push(s);
-        while(!mystack.empty()){
-            string src = mystack.top();
-            if(adj[src].size() == 0){
-                ans.push_back(src);
-                mystack.pop();
+        stack<string> st;
+        st.push("JFK");
+        vector<string> ans;
+        while(!st.empty()){
+            string node = st.top();
+            if(mp[node].size() == 0){
+                ans.push_back(node);
+                st.pop();
             }
             else{
-                auto dst = adj[src].begin();
-                mystack.push(*dst);
-                adj[src].erase(dst);
+                auto dst = mp[node].begin();
+                st.push(*dst);
+                mp[node].erase(dst);
             }
         }
         reverse(ans.begin(), ans.end());
