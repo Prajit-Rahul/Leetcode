4dfec6cee66436daf6cd87e34ac0f64fbcf9a8a2
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-13 12:54:17 -0800

    Time: 3 ms (49.29%) | Memory: 9.8 MB (13.46%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    bool recc(string &s, string &p, int ind1, int ind2, vector<vector<int>> &dp){
+    int recc(string &s, string &p, int ind1, int ind2, vector<vector<int>> &dp){
         int n = s.length(), m = p.length();
         if(ind2 >= m){
             return (ind1 == n);
         }
+        if(dp[ind1][ind2] != -1) return 0;
+        int match = ind1 < n && ((s[ind1] == p[ind2]) || (p[ind2] == '.'));
         bool ans = false;
-        bool match = ind1 < n && ((s[ind1] == p[ind2]) || p[ind2] == '.');
-        if(match){
-            ans = ans || recc(s, p, ind1+1, ind2+1, dp);
-        }
-        if(ind2+1 < m && p[ind2+1] == '*'){
-            ans = ans || recc(s, p, ind1, ind2+2, dp);
-            ans = ans || (match && recc(s, p, ind1+1, ind2, dp));
+        if(match) ans |= recc(s, p, ind1+1, ind2+1, dp);
+        if(ind2<m && p[ind2+1] == '*'){
+            if(match){
+                ans |= recc(s, p, ind1+1, ind2, dp);
+                ans |= recc(s, p, ind1+1, ind2+2, dp);
+            }
+            ans |= recc(s, p, ind1, ind2+2, dp);
         }
         return dp[ind1][ind2] = ans;
     }
     bool isMatch(string s, string p) {
         int n = s.length(), m = p.length();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         return recc(s, p, 0, 0, dp);
     }
 };
