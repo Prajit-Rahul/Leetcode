1783cbe1c8ef11a8d8ed36c4d30846031e772a4b
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-16 17:19:24 -0800

    Time: 41 ms (12.76%) | Memory: 87.8 MB (13.95%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    vector<int> pse(vector<int>& heights){
+    vector<int> nseList(vector<int> &heights){
         int n = heights.size();
-        vector<int> ps(n, -1);
-        stack<int> st;
-        for(int i=0; i<n; i++){
-            while(!st.empty() && heights[st.top()] >= heights[i]){
-                st.pop();
-            }
-            ps[i] = (st.empty())?-1:st.top();
-            st.push(i);
-        }
-        return ps;
-    }
-    vector<int> nse(vector<int>& heights){
-        int n = heights.size();
-        vector<int> ns(n);
+        vector<int> nse(n, 0);
         stack<int> st;
         for(int i=n-1; i>=0; i--){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                st.pop();
+            }
+            nse[i] = st.empty()?n:st.top();
+            st.push(i);
+        }
+        return nse;
+    }
+    vector<int> pseList(vector<int> &heights){
+        int n = heights.size();
+        vector<int> pse(n, -1);
+        stack<int> st;
+        for(int i=0; i<n; i++){
             while(!st.empty() && heights[st.top()] >= heights[i]){
                 st.pop();
             }
-            ns[i] = (st.empty())?n:st.top();
+            pse[i] = st.empty()?-1:st.top();
             st.push(i);
         }
-        return ns;
+        return pse;
     }
     int largestRectangleArea(vector<int>& heights) {
+        vector<int> nse = nseList(heights);
+        vector<int> pse = pseList(heights);
         int n = heights.size();
-        vector<int> ps = pse(heights);
-        vector<int> ns = nse(heights);
         int maxi = 0;
         for(int i=0; i<n; i++){
-            maxi = max(maxi, heights[i] * (ns[i] - ps[i] - 1));
+            int curr = (nse[i] - pse[i] - 1) * heights[i];
+            maxi = max(curr, maxi);
         }
         return maxi;
     }
