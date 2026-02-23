503ff15131405b8286770db9a344e7ff84215577
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-23 15:27:41 -0800

    Time: 35 ms (72.79%) | Memory: 53.5 MB (83.80%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    bool isPal(string &str, int ind, int i){
-        while(ind < i){
-            if(str[ind] != str[i]){
-                return false;
-            }
-            ind++;
-            i--;
+    bool isPal(string &s, int i, int j){
+        while(i<j){
+            if(s[i] != s[j]) return false;
+            i++, j--;
         }
         return true;
     }
-    void recc(string s, vector<string> &temp, int ind){
-        if(s.length() == ind){
+    void recc(string &s, int ind, vector<vector<string>> &ans, vector<string> &temp){
+        if(ind == s.length()){
             ans.push_back(temp);
             return;
         }
         for(int i=ind; i<s.length(); i++){
-            if(isPal(s, ind, i)){
+            if(isPal(s,ind,i)){
                 temp.push_back(s.substr(ind, i-ind+1));
-                recc(s, temp, i+1);
+                recc(s, i+1, ans, temp);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
         vector<string> temp;
-        recc(s, temp, 0);
+        recc(s, 0, ans, temp);
         return ans;
     }
 };
