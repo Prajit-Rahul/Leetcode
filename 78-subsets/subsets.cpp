0fe9503101c5d861c2faf886805fbbb9241c0bfd
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-23 11:32:05 -0800

    Time: 0 ms (100.00%) | Memory: 9.8 MB (82.65%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    void recc(vector<int>& nums, vector<vector<int>> &ans, int ind, vector<int> &temp){
+    void recc(vector<int>& nums, int ind, vector<vector<int>> &ans, vector<int>& temp){
         if(ind == nums.size()){
             ans.push_back(temp);
             return;
         }
         temp.push_back(nums[ind]);
-        recc(nums, ans, ind+1, temp);
+        recc(nums, ind+1, ans, temp);
         temp.pop_back();
-        recc(nums, ans, ind+1, temp);
+        recc(nums, ind+1, ans, temp);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        recc(nums, ans, 0, temp);
+        recc(nums, 0, ans, temp);
         return ans;
     }
 };
