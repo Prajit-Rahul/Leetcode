69dfc47fd8da1984f72646e577d219d24480ef8e
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-23 18:02:24 -0800

    Time: 251 ms (22.10%) | Memory: 12.6 MB (91.19%) - LeetSync

---
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool recc(vector<int>& matchsticks, int ind, vector<int> &side, int &length){
+        if(ind == matchsticks.size()){
+            return side[0] == side[1] && side[1] == side[2] && side[2] == side[3];
+        }
+        for(int i=0; i<4; i++){
+            if(side[i] + matchsticks[ind] > length) continue;
+            side[i] += matchsticks[ind];
+            if(recc(matchsticks, ind+1, side, length)) return true;
+            side[i] -= matchsticks[ind];
+        }
+        return false;
+    }
+    bool makesquare(vector<int>& matchsticks) {
+        int sum = 0;
+        vector<int> side(4, 0);
+        for(auto &it: matchsticks){
+            sum += it;
+        }
+        if(sum % 4 != 0) return false;
+        sum /= 4;
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
+        return recc(matchsticks, 0, side, sum);
+    }
+};
