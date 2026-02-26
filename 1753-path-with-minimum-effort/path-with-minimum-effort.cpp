c090cef85f56f6d8fdd757ea80754387cb12044a
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-26 10:48:05 -0800

    Time: 42 ms (87.76%) | Memory: 24.6 MB (42.03%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<>> q;
+        q.push({0, {0,0}});
         int n = heights.size(), m = heights[0].size();
-        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> q;
-        int dr[] = {0,1,-1,0};
-        int dc[] = {1,0,0,-1};
-        q.push({0,{0,0}});
-        dis[0][0] = 0;
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = 0;
+        vector<int> dr = {0,1,0,-1};
+        vector<int> dc = {-1,0,1,0};
         while(!q.empty()){
-            int r = q.top().second.first;
-            int c = q.top().second.second;
-            int wt = q.top().first;
+            int i = q.top().second.first;
+            int j = q.top().second.second;
+            int currAbs = q.top().first;
             q.pop();
-            for(int k = 0; k < 4; k++){
-                int nr = r + dr[k];
-                int nc = c + dc[k];
+            if(i==n-1 && j == m-1) return currAbs;
+            for(int k=0; k<4; k++){
+                int nr = i + dr[k];
+                int nc = j + dc[k];
                 if(nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
-                int effort = max(wt, abs(heights[r][c] -  heights[nr][nc]));
-                if(dis[nr][nc] > effort){
-                    dis[nr][nc] = effort;
-                    q.push({dis[nr][nc] , {nr, nc}});
+                int newAbs = abs(heights[i][j] - heights[nr][nc]);
+                newAbs = max(newAbs, currAbs);
+                if(newAbs < dist[nr][nc]){
+                    dist[nr][nc] = newAbs;
+                    q.push({newAbs,{nr, nc}});
                 }
             }
         }
-        return dis[n-1][m-1];
+        return -1;
     }
 };
