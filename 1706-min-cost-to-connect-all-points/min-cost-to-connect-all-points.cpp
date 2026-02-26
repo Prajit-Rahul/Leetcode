7b9254621bc2fc2b27f035d2e6fa66629a319fdf
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-26 13:46:05 -0800

    Time: 53 ms (80.67%) | Memory: 18.2 MB (80.25%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        vector<int> vis(n, 0);
+        vector<int> dist(n, INT_MAX);
+        vector<bool> vis(n, false);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        pq.push({0, 0});
         int min_cost = 0;
-        unordered_map<int, int> cache;
         while(!pq.empty()){
             auto [cost, node] = pq.top();
             pq.pop();
             if(vis[node]) continue;
             vis[node] = 1;
             min_cost += cost;
-            for(int v=0; v<n; v++){
-                if(!vis[v]){
-                    int dist = abs(points[v][0] - points[node][0]) + abs(points[v][1] - points[node][1]);
-                    if(cache.find(v) == cache.end() || cache[v] > dist){
-                        cache[v] = dist;
-                        pq.push({dist, v});
-                    } 
+            for(int i=0; i<n; i++){
+                int newCost = abs(points[i][0] - points[node][0]) + abs(points[i][1] - points[node][1]);
+                if(dist[i] > newCost){
+                    dist[i] = newCost;
+                    pq.push({dist[i], i});
                 }
             }
         }
