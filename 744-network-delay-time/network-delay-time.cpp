39675b3bbf9f57a940c06444cb812eea904f04da
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-26 11:25:49 -0800

    Time: 71 ms (93.47%) | Memory: 44.2 MB (90.87%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(auto &it: times){
-            adj[it[0]].push_back({it[1], it[2]});
+        vector<vector<pair<int,int>>> adjList(n+1);
+        for(auto &time: times){
+            adjList[time[0]].push_back({time[1], time[2]});
         }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> q;
         vector<int> dist(n+1, INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         dist[k] = 0;
-        pq.push({0, k});
-        while(!pq.empty()){
-            int node = pq.top().second;
-            int wt = pq.top().first;
-            pq.pop();
-            for(auto &it: adj[node]){
-                int adjNode = it.first;
-                int edgeWt = it.second;
-                if(wt + edgeWt < dist[adjNode]){
-                    dist[adjNode] = wt + edgeWt;
-                    pq.push({dist[adjNode], adjNode});
+        q.push({0, k});
+        while(!q.empty()){
+            int node = q.top().second;
+            int currTime = q.top().first;
+            q.pop();
+            for(auto &adj: adjList[node]){
+                int adjNode = adj.first;
+                int adjTime = adj.second;
+                if(dist[adjNode] > adjTime + currTime){
+                    dist[adjNode] = adjTime + currTime;
+                    q.push({dist[adjNode], adjNode});
                 }
             }
         }
-        int maxi = 0;
+        int ans = 0;
         for(int i=1; i<=n; i++){
-            maxi = max(maxi, dist[i]);
+            ans = max(ans, dist[i]);
         }
-        return maxi == INT_MAX?-1:maxi;
+        return ans==INT_MAX?-1:ans;
     }
 };
