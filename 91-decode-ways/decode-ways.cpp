d9297b044bf7c868271347854ed00cb079c75829
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-27 19:34:22 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (45.05%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -7,9 +7,11 @@ public:
         dp[0] = dp[1] = 1;
         for(int i=2; i<=n; i++){
             int one = s[i-1] - '0';
-            int two = std::stoi(s.substr(i-2, 2));
-            if(one != 0) dp[i] += dp[i-1];
-            if(two >= 10 && two <= 26) dp[i] += dp[i-2];
+            int two = 10*(s[i-2] - '0')+s[i-1]-'0'; 
+            if(one >= 1)
+                dp[i] = dp[i-1];
+            if(two >= 10 && two <= 26) 
+                dp[i] += dp[i-2];
         }
         return dp[n];
     }
