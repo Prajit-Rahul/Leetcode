044db0592a9bbd8169cc19f0c1b69b6bd33034e5
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-28 13:10:31 -0800

    Time: 19 ms (16.07%) | Memory: 18.7 MB (15.31%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    bool recc(string &s, unordered_set<string> &st, int ind, vector<int> &dp){
-        if(ind == s.length()) return true;
+    bool recc(string s, unordered_set<string> &st, int ind, vector<int> &dp){
+        if(ind == s.length()) return dp[ind] = true;
         if(dp[ind] != -1) return dp[ind];
-        for(int i=ind; i<s.length();i++){
-            if(st.count(s.substr(ind, i-ind+1)) > 0 && recc(s, st, i+1, dp)) 
-                return dp[ind] = true; 
+        for(int i=ind; i<s.length(); i++){
+            string word = s.substr(ind, i-ind+1);
+            if(st.find(word) != st.end() && recc(s, st, i+1, dp)){
+                return dp[ind] = true;
+            }
         }
         return dp[ind] = false;
+
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> st;
+        vector<int> dp(s.length()+1, -1);
         for(auto &it: wordDict){
             st.insert(it);
         }
-        vector<int> dp(s.length(), -1);
-        return recc(s, st, 0, dp);
+        recc(s, st, 0, dp);
+        return dp[0];
     }
 };
