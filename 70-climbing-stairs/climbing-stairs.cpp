991e3d6b49f13be1f8657d48c304b9067d84b5e6
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-27 16:48:58 -0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int recc(int n, vector<int> &dp){
-        if(n <= 0){
-            if(n == 0) return 1;
-            return 0;
-        }
+        if(n == 0) return 1;
+        if(n < 0) return 0;
         if(dp[n] != -1) return dp[n];
-        int l1 = recc(n-1, dp);
-        int l2 = recc(n-2, dp);
-        return dp[n] = l1+l2;
+        return dp[n] = recc(n-1, dp) + recc(n-2, dp);
     }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
