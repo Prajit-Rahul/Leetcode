48f76908213884bd46cbb703e0fed148236b70a4
Prajit Rahul <prajitrahul05@gmail.com>
2026-02-27 18:11:53 -0800

    Time: 0 ms (100.00%) | Memory: 10.8 MB (38.05%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int recc(vector<int> &nums, int ind, vector<int> &dp){
-        if(ind < 0) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int take = recc(nums, ind-1, dp);
-        int ntake = recc(nums, ind-2, dp) + nums[ind];
-        return dp[ind] = max(take, ntake);
+    // int recc(vector<int> &nums, int ind, vector<int> &dp){
+    //     if(ind < 0) return 0;
+    //     if(dp[ind] != -1) return dp[ind];
+    //     int take = recc(nums, ind-1, dp);
+    //     int ntake = recc(nums, ind-2, dp) + nums[ind];
+    //     return dp[ind] = max(take, ntake);
+    // }
+    // int rob(vector<int>& nums) {
+    //     vector<int> temp;
+    //     vector<int> dp1(nums.size(), -1), dp2(nums.size(), -1);
+    //     if(1 == nums.size()) return nums[0];
+    //     for(int i=1; i<nums.size(); i++){
+    //         temp.push_back(nums[i]);
+    //     }
+    //     int res1 = recc(temp, temp.size()-1, dp1);
+    //     nums.pop_back();
+    //     int res2 = recc(nums, nums.size()-1, dp2);
+    //     return max(res1, res2);
+    // }
+    int recc(vector<int> &nums, int l, int r){
+        vector<int> dp(nums.size(), 0);
+        for(int ind=l; ind<=r; ind++){
+            int take = (ind-1>=0)?dp[ind-1]:0;
+            int ntake = (ind-2>=0)?dp[ind-2]:0;
+            ntake += nums[ind];
+            dp[ind] = max(take, ntake);
+        }
+        return dp[r];
     }
     int rob(vector<int>& nums) {
-        vector<int> temp;
-        vector<int> dp1(nums.size(), -1), dp2(nums.size(), -1);
-        if(1 == nums.size()) return nums[0];
-        for(int i=1; i<nums.size(); i++){
-            temp.push_back(nums[i]);
-        }
-        int res1 = recc(temp, temp.size()-1, dp1);
-        nums.pop_back();
-        int res2 = recc(nums, nums.size()-1, dp2);
+        if(nums.size() == 1) return nums[0];
+        int res1 = recc(nums, 0, nums.size()-2);
+        int res2 = recc(nums, 1, nums.size()-1);
         return max(res1, res2);
     }
 };
