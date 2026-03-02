14ba90ec67477a7c6447494bb5989f45158b6a94
Prajit Rahul <prajitrahul05@gmail.com>
2026-03-02 10:09:30 -0800

    Time: 4 ms (77.98%) | Memory: 15.6 MB (41.31%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    int recc(vector<int>& nums, int sum, int ind, vector<vector<int>> &dp){
+        if(ind < 0){
+            if(sum == 0) return 1;
+            return 0;
+        }
+        if(dp[ind][sum] != -1) return dp[ind][sum];
+        int take = 0;
+        if(sum-nums[ind]>=0)
+            take = recc(nums, sum-nums[ind], ind-1, dp);
+        int ntake = recc(nums, sum, ind-1, dp);
+        return dp[ind][sum] = take + ntake;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = 0;
+        for(auto &it: nums){
+            sum += it;
+        }
+        sum -= target;
+        if(sum < 0 || sum%2 != 0) return 0;
+        sum /= 2;
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+        return recc(nums, sum, n-1, dp);
+    }
     // int recc(vector<int>& nums, int target, int ind, vector<vector<int>> &dp){
     //     if(ind == 0){
     //         if(nums[ind] == 0 && target == 0) return 2;
@@ -25,26 +49,26 @@ public:
     //     vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
     //     return recc(nums, sum, n-1, dp);
     // }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int sum = 0;
-        for(auto &it: nums){
-            sum += it;
-        }
-        sum -= target;
-        if(sum < 0 || sum%2 != 0) return 0;
-        sum /= 2;
-        vector<vector<unsigned>> dp(n+1, vector<unsigned>(sum+1, 0));
-        dp[0][0] = 1;
-        if(sum >= nums[0]) dp[0][nums[0]] = 1;
-        if(nums[0] == 0) dp[0][0] = 2;
-        for(int ind=1; ind<n; ind++){
-            for(int tar = 0; tar<=sum; tar++){
-                if(tar >= nums[ind])
-                    dp[ind][tar] += dp[ind-1][tar-nums[ind]];
-                dp[ind][tar] += dp[ind-1][tar];
-            }
-        }
-        return dp[n-1][sum];
-    }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     int n = nums.size();
+    //     int sum = 0;
+    //     for(auto &it: nums){
+    //         sum += it;
+    //     }
+    //     sum -= target;
+    //     if(sum < 0 || sum%2 != 0) return 0;
+    //     sum /= 2;
+    //     vector<vector<unsigned>> dp(n+1, vector<unsigned>(sum+1, 0));
+    //     dp[0][0] = 1;
+    //     if(sum >= nums[0]) dp[0][nums[0]] = 1;
+    //     if(nums[0] == 0) dp[0][0] = 2;
+    //     for(int ind=1; ind<n; ind++){
+    //         for(int tar = 0; tar<=sum; tar++){
+    //             if(tar >= nums[ind])
+    //                 dp[ind][tar] += dp[ind-1][tar-nums[ind]];
+    //             dp[ind][tar] += dp[ind-1][tar];
+    //         }
+    //     }
+    //     return dp[n-1][sum];
+    // }
 };
