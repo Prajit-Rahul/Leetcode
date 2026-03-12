0ab80fd092f8bb354bfa51e53bce5ed669d295fb
Prajit Rahul <prajitrahul05@gmail.com>
2026-03-11 23:55:27 -0700

    Time: 3 ms (22.77%) | Memory: 9.7 MB (81.26%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 != str2 + str1)
+            return "";
+        return str2.substr(0, gcd(str1.size(), str2.size()));
+    }
+};
