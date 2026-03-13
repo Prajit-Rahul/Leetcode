1590bb604442b602609b0ecb754c1f3715dd2bf3
Prajit Rahul <prajitrahul05@gmail.com>
2026-03-13 11:27:57 -0700

    Time: 199 ms (44.41%) | Memory: 14 MB (29.51%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    int recc(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
+        if(i>j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = 0;
+        for(int k=i; k<=j; k++){
+            ans = max(ans, nums[i-1] * nums[k] * nums[j+1] + recc(nums, i, k-1, dp) + recc(nums, k+1, j, dp));
+        }
+        return dp[i][j] = ans;
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        int c = nums.size();
+        vector<vector<int>> dp(c+1, vector<int>(c+1, -1));
+        return recc(nums, 1, c-2, dp);
+    }
     // int recc(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
     //     if(i > j) return 0;
     //     if(dp[i][j] != -1) return dp[i][j];
@@ -17,32 +33,22 @@ public:
     //     vector<vector<int>> dp(c+1, vector<int>(c+1, -1));
     //     return recc(nums, 1, c-2, dp);
     // }
-    // int recc(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
-    //     if(i > j) return 0;
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     int maxi = 0;
-    //     for(int ind = i; ind <= j; ind++){
-    //         int cost = nums[i-1] * nums[ind] * nums[j+1] + recc(nums, i, ind-1, dp) + recc(nums, ind+1, j, dp);
-    //         maxi = max(cost, maxi);
+    // int maxCoins(vector<int>& nums) {
+    //     nums.insert(nums.begin(), 1);
+    //     nums.push_back(1);
+    //     int c = nums.size();
+    //     vector<vector<int>> dp(c+1, vector<int>(c+1, 0));
+    //     for(int i=c-2; i>=1; i--){
+    //         for(int j=1; j<=c-2; j++){
+    //             if(i > j) continue;
+    //             int maxi = 0;
+    //             for(int ind = i; ind <= j; ind++){
+    //                 int cost = nums[i-1] * nums[ind] * nums[j+1] + dp[i][ind-1] + dp[ind+1][j];
+    //                 maxi = max(cost, maxi);
+    //             }
+    //             dp[i][j] = maxi;
+    //         }
     //     }
-    //     return dp[i][j] = maxi;
+    //     return dp[1][c-2];
     // }
-    int maxCoins(vector<int>& nums) {
-        nums.insert(nums.begin(), 1);
-        nums.push_back(1);
-        int c = nums.size();
-        vector<vector<int>> dp(c+1, vector<int>(c+1, 0));
-        for(int i=c-2; i>=1; i--){
-            for(int j=1; j<=c-2; j++){
-                if(i > j) continue;
-                int maxi = 0;
-                for(int ind = i; ind <= j; ind++){
-                    int cost = nums[i-1] * nums[ind] * nums[j+1] + dp[i][ind-1] + dp[ind+1][j];
-                    maxi = max(cost, maxi);
-                }
-                dp[i][j] = maxi;
-            }
-        }
-        return dp[1][c-2];
-    }
 };
