261571f1691b5e05aa6cba97e80d28bc9ea19ff5
Prajit Rahul <prajitrahul05@gmail.com>
2026-03-14 10:09:39 -0700

    Time: 18 ms (68.94%) | Memory: 28.9 MB (90.68%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
-    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        long long ans = 0;
-        int n = workerTimes.size();
-        priority_queue<pair<long long,pair<int,int>>, vector<pair<long long,pair<int,int>>>, greater<pair<long long,pair<int,int>>>> pq;
-        for(int i=0; i<n; i++){
-            pq.push({workerTimes[i], {i,1}});
-        }
-        vector<int> workTime(n, 0);
-        for(int i=0; i<mountainHeight; i++){
-            long long time = pq.top().first;
-            auto [worker, ht] = pq.top().second;
-            pq.pop();
-            ans = time;
-            pq.push({time + 1ll*(ht+1)*workerTimes[worker], {worker, ht+1}});
-        }
-        return ans;
+    long long block(long long time, int t){
+        long long val = (2 * time)/t;
+        long long k = (sqrt(1 + 4 * val) - 1)/2;
+        return k;
     }
+    bool can(long long time, int mountainHeight, vector<int>& workerTimes){
+        long long height = 0;
+        for(int t: workerTimes){
+            height += block(time, t);
+            if(height >= mountainHeight) return true;
+        }
+        return false;
+    }
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long left = 1, right = 10e15;
+        int n = workerTimes.size();
+        while(left < right){
+            long long mid = left + (right-left)/2;
+            if(can(mid, mountainHeight, workerTimes)){
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return right;
+    }
+    // long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+    //     long long ans = 0;
+    //     int n = workerTimes.size();
+    //     priority_queue<pair<long long,pair<int,int>>, vector<pair<long long,pair<int,int>>>, greater<pair<long long,pair<int,int>>>> pq;
+    //     for(int i=0; i<n; i++){
+    //         pq.push({workerTimes[i], {i,1}});
+    //     }
+    //     vector<int> workTime(n, 0);
+    //     for(int i=0; i<mountainHeight; i++){
+    //         long long time = pq.top().first;
+    //         auto [worker, ht] = pq.top().second;
+    //         pq.pop();
+    //         ans = time;
+    //         pq.push({time + 1ll*(ht+1)*workerTimes[worker], {worker, ht+1}});
+    //     }
+    //     return ans;
+    // }
 };
