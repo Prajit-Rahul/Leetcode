6c0b1e760d9e73279d4f46bec93ee4660799b1a4
Prajit Rahul <prajitrahul05@gmail.com>
2026-03-30 13:35:51 -0700

    Time: 4 ms (53.34%) | Memory: 10 MB (44.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int> mp1, mp2;
-        int k = s1.length();
-        for(auto &it: s1){
-            mp1[it]++;
+        vector<int> freq1(26, 0), freq2(26, 0);
+        int k = s1.length(), n = s2.length(), match = 0;
+        int l = 0, r = k;
+        if(n<k) return false;
+        for(int i=0; i<k; i++){
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
         }
-        for(int i=0; i<s2.length(); i++){
-            mp2[s2[i]]++;
-            if(i >= k){
-                mp2[s2[i-k]]--;
-                if(mp2[s2[i-k]] == 0) mp2.erase(s2[i-k]);
-            }
-            if(mp1 == mp2) return true;
+        for(int i=0; i<26; i++){
+            if(freq1[i] == freq2[i])
+                match++;
+        }
+        if(match == 26) return true;
+        while(r<n){
+            freq2[s2[l] - 'a']--;
+            if(freq2[s2[l] - 'a'] == freq1[s2[l] - 'a']) match++;
+            else if(freq2[s2[l] - 'a'] + 1 == freq1[s2[l] - 'a']) match--;
+            freq2[s2[r] - 'a']++;
+            if(freq2[s2[r] - 'a'] == freq1[s2[r] - 'a']) match++;
+            else if(freq2[s2[r] - 'a'] - 1 == freq1[s2[r] - 'a']) match--;
+            if(match == 26) return true;
+            l++, r++;
         }
         return false;
     }
