d4806735228091dc742679e18106d7eb70bac82a
Prajit Rahul <prajitrahul05@gmail.com>
2026-03-29 19:27:30 -0700

    Time: 19 ms (11.94%) | Memory: 11 MB (39.68%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    // int characterReplacement(string s, int k) {
-    //     int l = 0, r = 0, n = s.length();
-    //     vector<int> mp(26,0);
-    //     int maxFreq = 0, maxLen = 0;
-    //     while(r<n){
-    //         maxFreq = max(maxFreq, ++mp[s[r]-'A']);
-    //         if((r-l+1) - maxFreq > k){
-    //             mp[s[l]-'A']--;
-    //             l++;
-    //         }
-    //         maxLen = max(maxLen, r-l+1);
-    //         r++;
-    //     }
-    //     return maxLen;
-    // }
     int characterReplacement(string s, int k) {
-        int l = 0, r = 0, n = s.length();
-        int maxCount = 0, maxFreq = 0;
-        unordered_map<char, int> mp;
-        while(r < n){
-            maxFreq = max(maxFreq, ++mp[s[r]]);
+        int l = 0, r = 0, maxi = 0, n = s.length(), maxFreq = 0;
+        unordered_map<int,int> mp;
+        while(r<n){
+            mp[s[r]]++;
+            maxFreq = max(mp[s[r]], maxFreq);
             if((r-l+1) - maxFreq <= k){
-                maxCount = max(maxCount, (r-l+1));
+                maxi = max(r-l+1, maxi);
             }
             else{
                 mp[s[l]]--;
@@ -30,6 +15,6 @@ public:
             }
             r++;
         }
-        return maxCount;
+        return maxi;
     }
 };
